0d0e82be3b9641b0d215fd50e683ed43f2771a8f
Hardik Singh <134148118+hardiksinghh18@users.noreply.github.com>
2024-04-15 15:00:41 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -317,8 +317,8 @@ app.post('/deleteblog', async (req, res) => {
 
 app.get('/logout', verifyuser, async (req, res) => {
     try {
-        res.clearCookie('accessToken')
-        res.clearCookie('refreshToken')
+        res.clearCookie('accessToken',{httpOnly: true, secure: true, sameSite: 'none'})
+        res.clearCookie('refreshToken',{httpOnly: true, secure: true, sameSite: 'none'})
         res.json({ valid: false, message: 'Logged Out' })
     } catch (error) {
         console.log(error)
