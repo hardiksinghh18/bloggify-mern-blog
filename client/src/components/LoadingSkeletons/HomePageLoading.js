eada793231dfdb83e390e7610a50dd2a9e617c8d
Hardik Singh <134148118+hardiksinghh18@users.noreply.github.com>
2024-04-18 18:37:56 +0530

    Create HomePageLoading.js

---
+++ client/src/components/LoadingSkeletons/HomePageLoading.js
@@ -0,0 +1,56 @@
+import React from 'react'
+
+const HomePageLoading = () => {
+  return (
+    <>
+          
+
+
+
+       <div className=" w-screen m-4 px-2 flex flex-col gap-2 justify-center items-center h-40 lg:h-80 rounded-2xl animate-pulse">
+        <div className="w-screen h-40 bg-gray-300 rounded-lg"></div>
+       
+        
+      </div>
+
+
+
+      <div className='flex flex-col  px-8   w-scrren gap-2'>
+                <div className="recommendedCard   flex gap-2 justify-center items-center rounded-2xl animate-pulse">
+
+                    <div className="w-16 h-12 bg-gray-300 rounded-lg"></div>
+                    <div className="w-full px-2 py-1">
+                        <div className="w-full bg-gray-300 h-3 mb-2 rounded"></div>
+                        <div className="w-20 bg-gray-300 h-3 rounded"></div>
+                    </div>
+
+
+                </div>
+                <div className="recommendedCard  flex gap-2 justify-center items-center rounded-2xl animate-pulse">
+
+                    <div className="w-16 h-12 bg-gray-300 rounded-lg"></div>
+                    <div className="w-full px-2 py-1">
+                        <div className="w-full bg-gray-300 h-3 mb-2 rounded"></div>
+                        <div className="w-20 bg-gray-300 h-3 rounded"></div>
+                    </div>
+
+
+                </div>
+                <div className="recommendedCard  flex gap-2 justify-center items-center rounded-2xl animate-pulse">
+
+                    <div className="w-16 h-12 bg-gray-300 rounded-lg"></div>
+                    <div className="w-full px-2 py-1">
+                        <div className="w-full bg-gray-300 h-3 mb-2 rounded"></div>
+                        <div className="w-20 bg-gray-300 h-3 rounded"></div>
+                    </div>
+
+
+                </div>
+
+            </div>
+     
+    </>
+  )
+}
+
+export default HomePageLoading;
