ca0814fdf97e4e4aab68b13c6a362dd81846d891
Hardik Singh <134148118+hardiksinghh18@users.noreply.github.com>
2024-04-18 14:54:30 +0530

    Update verifyuser.js

--- backend/middleware/verifyuser.js
+++ backend/middleware/verifyuser.js
@@ -43,8 +43,8 @@ const renewToken = (req, res) => {
                 return res.json({ valid: false, message: 'Invalid Refresh Token' });
             } else {
                 const accessToken = jwt.sign({ email: decoded.email },
-                    process.env.ACCESS_TOKEN_KEY ,{ expiresIn: '90d' })
-                res.cookie('accessToken', accessToken, { maxAge: 90 * 24 * 60 * 60 * 1000 })
+                    process.env.ACCESS_TOKEN_KEY ,{ expiresIn: '1d' })
+                res.cookie('accessToken', accessToken, { maxAge: 24 * 60 * 60 * 1000, path: "/", httpOnly: true, secure: true, sameSite: 'None' })
                 exist = true;
             }
         })
