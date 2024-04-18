15ccdbccd162437f77ee010e86c47d433589d373
Hardik Singh <134148118+hardiksinghh18@users.noreply.github.com>
2024-04-18 18:38:34 +0530

    Update LoadingSkeleton.js

--- client/src/components/LoadingSkeletons/LoadingSkeleton.js
+++ client/src/components/LoadingSkeletons/LoadingSkeleton.js
@@ -1,29 +1,66 @@
 import React from 'react'
 
 const LoadingSkeleton = () => {
-  return (
-    <>
-      
-<div role="status" className="space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex-row md:items-center px-24 flex-col    h-screen flex justify-center items-center">
-    <div className="flex items-center justify-center w-full h-48 bg-gray-300 rounded sm:w-96 ">
-        <svg className=" text-gray-200 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-            <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
-        </svg>
-    </div>
-    <div className="w-full">
-        <div className="h-2.5 bg-gray-200 rounded-full w-48 mb-4"></div>
-        <div className="h-2 bg-gray-200 rounded-full  max-w-[480px] mb-2.5"></div>
-        <div className="h-2 bg-gray-200 rounded-full  mb-2.5"></div>
-        <div className="h-2 bg-gray-200 rounded-full  max-w-[440px] mb-2.5"></div>
-        <div className="h-2 bg-gray-200 rounded-full  max-w-[460px] mb-2.5"></div>
-        <div className="h-2 bg-gray-200 rounded-full  max-w-[360px]"></div>
-    </div>
-    <span className="sr-only">Loading...</span>
-</div>
+    return (
+        <div div className='flex flex-col w-screen sm:flex-row min-h-80 px-8  sm:px-16  sm:justify-start sm:items-start  mt-8 '>
+
+            <div className="recommendedCard w-fit  sm:px-8  flex flex-col gap-2 justify-center items-center   rounded-2xl animate-pulse">
+                <div className='flex w-full ' >
+                    <div className="w-16 h-12 bg-gray-300 rounded-full"></div>
+                    <div className="w-full px-2 py-1">
+                        <div className="w-full bg-gray-300 h-3 mb-2 rounded"></div>
+                        <div className="w-20 bg-gray-300 h-3 rounded"></div>
+                    </div>
+                </div>
+                <div className="flex w-full px-2 pb-4 justify-between items-end">
+                    <div className="flex flex-col w-full">
+                        <div className="w-full bg-gray-300 h-3 mb-2 rounded"></div>
+                        <div className="w-20 bg-gray-300 h-3 rounded"></div>
+                    </div>
+                    {/* <div className="text-sm text-gray-400 hover:border-b-2">Read More</div> */}
+                </div>
+            </div>
 
 
-    </>
-  )
+            <div className='flex flex-col  sm:px-8   sm:w-2/3 gap-2'>
+                <div className="recommendedCard   flex gap-2 justify-center items-center rounded-2xl animate-pulse">
+
+                    <div className="w-16 h-12 bg-gray-300 rounded-lg"></div>
+                    <div className="w-full px-2 py-1">
+                        <div className="w-full bg-gray-300 h-3 mb-2 rounded"></div>
+                        <div className="w-20 bg-gray-300 h-3 rounded"></div>
+                    </div>
+
+
+                </div>
+                <div className="recommendedCard  flex gap-2 justify-center items-center rounded-2xl animate-pulse">
+
+                    <div className="w-16 h-12 bg-gray-300 rounded-lg"></div>
+                    <div className="w-full px-2 py-1">
+                        <div className="w-full bg-gray-300 h-3 mb-2 rounded"></div>
+                        <div className="w-20 bg-gray-300 h-3 rounded"></div>
+                    </div>
+
+
+                </div>
+                <div className="recommendedCard  flex gap-2 justify-center items-center rounded-2xl animate-pulse">
+
+                    <div className="w-16 h-12 bg-gray-300 rounded-lg"></div>
+                    <div className="w-full px-2 py-1">
+                        <div className="w-full bg-gray-300 h-3 mb-2 rounded"></div>
+                        <div className="w-20 bg-gray-300 h-3 rounded"></div>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+    )
 }
 
 export default LoadingSkeleton
